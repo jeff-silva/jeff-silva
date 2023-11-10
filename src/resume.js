3a91bb9efb16e762670752036d10e08e6455a00a
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-11-09 22:09:21 -0300

    markdown curriculum

--- src/resume.js
+++ src/resume.js
@@ -23,16 +23,21 @@ export const markdownWrite = () => {
     },
   };
   
-  const textSpacer = ({ left='', right='', size=boxWidth, str='·' }) => {
+  const textSpacer = ({ left='', right='', size=boxWidth, str='·', offset=0 }) => {
     const fakeL = (new JSDOM(left)).window.document.body.textContent;
     const fakeR = (new JSDOM(right)).window.document.body.textContent;
-    const spacerSize = size - fakeL.length - fakeR.length;
+    const spacerSize = size - fakeL.length - fakeR.length - offset;
     return left +' '+ (str.repeat(spacerSize)) +' '+ right;
   };
 
   const textCenter = ({ text, str='~', width=boxWidth }) => {
     const sideSize = Math.ceil((width - text.length) / 2);
     return str.repeat(sideSize) + ` ${text} ` + str.repeat(sideSize);
+  };
+
+  const textOrLink = ({ text='', url='' }) => {
+    if (url) return `<a href="${url}" target="_blank">${text}</a>`;
+    return text;
   };
 
   const sectionAdd = (title, callback) => {
@@ -47,8 +52,9 @@ export const markdownWrite = () => {
 
   // Bio
   sectionAdd(data.name.toUpperCase(), (lines, options) => {
-    lines.push(data.description);
-    lines.push(`:round_pushpin: ${data.place}`);
+    lines.push(`👨🏻‍💻 ${data.description}`);
+    lines.push(`🌎 ${data.place}`);
+    if (data.openToWork) lines.push(`🟢 Open to work`);
     lines.push('', data.bio);
   });
 
@@ -75,9 +81,8 @@ export const markdownWrite = () => {
   // Skills
   sectionAdd('SKILLS', (lines, options) => {
     data.skills.map((item) => {
-      const repeatSize = 5;
-      const stars = '='.repeat(repeatSize).repeat(item.rating) + ' '.repeat(repeatSize).repeat(5-item.rating);
-      lines.push(textSpacer({ left: item.name, right: `${item.rating}/5 ${stars}` }));
+      const stars = '⭐'.repeat(item.rating) + ' '.repeat(5-item.rating);
+      lines.push(textSpacer({ left: item.name, right: `${item.rating}/5 ${stars}`, offset: 7 }));
     });
   });
 
@@ -94,17 +99,26 @@ export const markdownWrite = () => {
         (item.date_final ? dayjs(item.date_final).format('YYYY') : 'Atualmente'),
       ].join(' ~ ');
 
+      const itemName = textOrLink({ text: item.name, url: item.url });
+
       lines.push(textSpacer({
-        left: `${item.name} | ${item.job}`,
+        left: `${itemName} · ${item.job}`,
         right: dates,
       }));
-      if (item.description) lines.push(item.description);
+
+      if (item.description) {
+        lines.push(item.description);
+      }
+
       lines.push(`Stack: ${item.stack.join(', ')}`);
       
       if (item.projects.length>0) {
         lines.push('', 'Projetos executados:');
         item.projects.map((project) => {
-          lines.push(`• <a href="${project.url}" target="_blank">${project.name}</a>`);
+          lines.push(textSpacer({
+            left: textOrLink({ text: project.name, url: project.url }),
+            right: project.stack.join(', '),
+          }));
         });
       }
     });
@@ -127,6 +141,7 @@ export const data = {
     'I am an enthusiast of simple tools that help me focus only on the business rules of the system.',
   ].join("\n"),
   place: "Belo Horizonte / MG",
+  openToWork: true,
   links: [
     {
       name: "Website",
@@ -210,9 +225,10 @@ export const data = {
       description: "Desenvolvimento de projetos freelancer e pessoais",
       job: "Dev Fullstack",
       active: true,
+      url: "",
       date_start: "2010-01-01",
       date_final: false,
-      stack: [ "PHP", "Laravel", "Wordpress", "Git", "Docker", "Vue", "Nuxt" ],
+      stack: [ "PHP", "Laravel", "Wordpress", "Git", "Docker", "Vue", "Nuxt", "Elementor", "Bootstrap" ],
       projects: [
         {
           name: "Blog Banco da Amazônia",
@@ -220,12 +236,14 @@ export const data = {
             Feito utilizando Wordpress + Elementor com elementos personalizados.
             Feito em parceria com a Partners Comunicação Pro Business, 2019.`,
           url: "https://basablog.com.br",
+          stack: [ "Wordpress" ],
           images: [],
         },
         {
           name: "Don Desenvolvimento",
           description: `Alterações no layout original e manutenções pontuais utilizando Wordpress e Vue. Ano: 2018`,
           url: "https://dondesenvolvimento.com.br",
+          stack: [ "Wordpress", "Vue", "Elementor", "Bootstrap" ],
           images: [],
         },
         {
@@ -233,6 +251,7 @@ export const data = {
           description: `E-commerce para vendas de produtos voltados para skatistas.
             Criado com Wordpress + Woocommerce + Elementor com elementos customizados, 2018.`,
           url: "https://cocksskate.com.br",
+          stack: [ "Wordpress", "Vue", "Elementor", "Bootstrap" ],
           images: [],
         },
         {
@@ -241,18 +260,21 @@ export const data = {
             Feito utilizando Wordpress + Elementor com elementos personalizados.
             Feito em parceria com a Partners Comunicação Pro Business, 2019.`,
           url: "https://orbitae.com.br",
+          stack: [ "Wordpress", "Vue", "Elementor", "Bootstrap" ],
           images: [],
         },
         {
           name: "Habitat Group",
           description: `Clínica veterinária, utilizando Wordpress + Elementor com elementos customizados, 2019.`,
           url: "https://habitatgroup.com.br",
+          stack: [ "Wordpress", "Vue", "Elementor", "Bootstrap" ],
           images: [],
         },
         {
           name: "Eiriz Saúde",
           description: `Site em parceria com a Subtl - Agência de marketing em Portugal, 2017`,
           url: "http://eiriz-saude.unbox.pt/",
+          stack: [ "Wordpress", "Vue", "Elementor", "Bootstrap" ],
           images: [],
         },
       ],
@@ -262,6 +284,7 @@ export const data = {
       description: "Sistema de locação por temporada para imóveis na Austrália",
       job: "Dev Fullstack",
       active: true,
+      url: "https://www.searchandstay.com",
       date_start: "2022-12-01",
       date_final: "2023-11-01",
       stack: [ "Vue", "Nuxt", "Javascript", "Node.js", "Laravel", "Git", "Docker" ],
@@ -272,6 +295,7 @@ export const data = {
       description: "Plataforma de ensino à distância voltada para a área de agronegócio",
       job: "Dev Frontend",
       active: true,
+      url: "",
       date_start: "2022-09-01",
       date_final: "2022-12-01",
       stack: [ "React", "Javascript", "Laravel", "Node.js", "Git", "CSS", "HTML" ],
@@ -282,6 +306,7 @@ export const data = {
       description: "Plataforma de ensino à distância",
       job: "Dev Frontend",
       active: true,
+      url: "",
       date_start: "2022-04-01",
       date_final: "2022-08-01",
       stack: [ "Vue.js", "Javascript", "Node.js", "CSS", "HTML" ],
@@ -292,6 +317,7 @@ export const data = {
       description: "",
       job: "Dev Fullstack",
       active: true,
+      url: "",
       date_start: "2020-10-01",
       date_final: "2022-04-01",
       stack: [ "Vue.js", "Nuxt", "PHP", "Laravel", "MySQL", "Javascript", "CSS", "HTML" ],
@@ -300,18 +326,21 @@ export const data = {
           name: "Assinar ou Comprar",
           description: "",
           url: "https://assinaroucomprar.com.br/",
+          stack: [ "Nuxt", "Rest API", "Laravel" ],
           images: [],
         },
         {
           name: "Rede Clipping",
           description: "",
           url: "https://redeclipping.com.br/",
+          stack: [ "Laravel" ],
           images: [],
         },
         {
-          name: "EPA",
+          name: "EPA Supermercados",
           description: "",
           url: "https://epa.com.br",
+          stack: [ "Wordpress" ],
           images: [],
         },
       ],
@@ -321,26 +350,52 @@ export const data = {
       description: "",
       job: "Dev Fullstack",
       active: true,
+      url: "",
       date_start: "2020-08-01",
       date_final: "2020-10-01",
       stack: [ "Vue.js", "PHP", "Wordpress", "MySQL", "Javascript", "CSS", "HTML" ],
-      projects: [],
+      projects: [
+        {
+          name: "Samarco",
+          description: "",
+          url: "",
+          stack: [ "Wordpress", "Bootstrap", "Elementor", "Vue" ],
+          images: [],
+        },
+        {
+          name: "Faveni",
+          description: "",
+          url: "",
+          stack: [ "Wordpress", "Bootstrap", "Elementor", "Vue" ],
+          images: [],
+        },
+      ],
     },
     {
       name: "Codificar",
       description: "",
       job: "Dev Fullstack",
       active: true,
+      url: "",
       date_start: "2017-02-01",
       date_final: "2017-10-01",
       stack: [ "Vue.js", "PHP", "MySQL", "Laravel", "Javascript", "CSS", "HTML" ],
-      projects: [],
+      projects: [
+        {
+          name: "Rescon",
+          description: "",
+          url: "",
+          stack: [ "Framework desconhecido", "PHP", "Vue" ],
+          images: [],
+        },
+      ],
     },
     {
       name: "Agência de Criação",
-      description: "",
+      description: "Prestação de serviço terceirizado para a Vale do Rio Doce",
       job: "Dev Fullstack",
       active: true,
+      url: "",
       date_start: "2015-01-01",
       date_final: "2017-01-01",
       stack: [ "Angular", "Vue.js", "PHP", "MySQL", "Javascript", "JQuery", "CSS", "HTML" ],
@@ -349,24 +404,28 @@ export const data = {
           name: "ASSEMG",
           description: "Site com CMS totalmente personalizado, 2014.",
           url: "https://web.archive.org/web/20170612004919/http://assemg.org/",
+          stack: [ ],
           images: [],
         },
         {
           name: "Bretas e Reis",
           description: "Website com CMS personalizado para a agência, 2015.",
           url: "http://www.brettasereis.adv.br",
+          stack: [ ],
           images: [],
         },
         {
           name: "Cantor Beto Santos",
           description: "Desenvolvimento de website com CMS personalizado para a agência, 2015",
           url: "http://betosantos.net",
+          stack: [ ],
           images: [],
         },
         {
           name: "Águas do Acuruí",
           description: "Serviço prestado para Agência de criação, 2015",
           url: "http://aguasdoacurui.com.br/",
+          stack: [ ],
           images: [],
         },
       ],
@@ -376,26 +435,52 @@ export const data = {
       description: "",
       job: "Dev Fullstack",
       active: true,
+      url: "",
       date_start: "2012-02-01",
       date_final: "2013-01-01",
       stack: [ "PHP", "MySQL", "Javascript", "JQuery", "CSS", "HTML" ],
-      projects: [],
+      projects: [
+        {
+          name: "Calculadora de Gastos de Obra",
+          description: "Sistema para cálculo de valor total de obra",
+          url: "",
+          stack: [ "PHP", "MySQL", "CSS", "JQuery" ],
+          images: [],
+        },
+        {
+          name: "Database de fornecedores",
+          description: "Banco de dados de fornecedores com diversos tipos de filtros",
+          url: "",
+          stack: [ "PHP", "MySQL", "CSS", "JQuery" ],
+          images: [],
+        },
+      ],
     },
     {
       name: "Netranet Networking",
       description: "",
       job: "Dev Fullstack",
       active: true,
+      url: "",
       date_start: "2011-11-01",
       date_final: "2012-01-01",
       stack: [ "PHP", "MySQL", "Javascript", "JQuery", "CSS", "HTML" ],
-      projects: [],
+      projects: [
+        {
+          name: "Estrela Rural",
+          description: "Leilão de cavalos e bois",
+          url: "",
+          stack: [ "PHP", "MySQL", "CSS", "JQuery" ],
+          images: [],
+        },
+      ],
     },
     {
       name: "Web BH Escola de Informática",
       description: "",
       job: "Instrutor",
       active: true,
+      url: "",
       date_start: "2010-01-01",
       date_final: "2011-01-01",
       stack: [ "PHP", "MySQL", "Javascript", "CSS", "ActionScript", "HTML" ],
