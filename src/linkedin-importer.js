16afe3e55ef99122092fcdc627dfe8b630d2e469
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-11-11 10:01:45 -0300

    linkedin scrapper

--- src/linkedin-importer.js
+++ src/linkedin-importer.js
@@ -6,6 +6,7 @@ export default async () => {
 
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
   );
@@ -42,13 +43,14 @@ export default async () => {
 
   try {
     let profile = {};
+
     profile.name = await querySelectorAttr(page, ".top-card-layout__title");
     profile.stack = await querySelectorAttr(page, ".top-card-layout__headline");
     profile.place = await querySelectorAttr(page, ".top-card-layout__first-subline > *:nth-child(1)");
     profile.bio = await querySelectorAttr(page, ".core-section-container__content");
 
     profile.experience = await querySelectorAll(page, ".experience__list > li", async (elem) => {
-      let data = { id: null };
+      let data = {};
       data.name = await querySelectorAttr(elem, ":scope > div > *:nth-child(1)");
       data.job = await querySelectorAttr(elem, ":scope > div > *:nth-child(1)");
       data.company = await querySelectorAttr(elem, ":scope > div > *:nth-child(2)");
@@ -56,32 +58,28 @@ export default async () => {
       data.date_start = await querySelectorAttr(elem, ":scope > div > *:nth-child(3) .date-range time:nth-child(1)");
       data.date_final = await querySelectorAttr(elem, ":scope > div > *:nth-child(3) .date-range time:nth-child(2)");
       data.place = await querySelectorAttr(elem, ":scope > div > *:nth-child(3) > *:nth-child(3)");
-      data.id = slugify(`${data.company}-${data.job}`);
       return data;
     });
 
     profile.education = await querySelectorAll(page, ".education__list > li", async (elem) => {
-      let data = { id: null };
+      let data = {};
       data.name = await querySelectorAttr(elem, ":scope > div > *:nth-child(1)");
       data.school_name = await querySelectorAttr(elem, ":scope > div > *:nth-child(1)");
       data.course_name = await querySelectorAttr(elem, ":scope > div > *:nth-child(2)");
       data.date_start = await querySelectorAttr(elem, ":scope > div > *:nth-child(3) .date-range time:nth-child(1)");
       data.date_final = await querySelectorAttr(elem, ":scope > div > *:nth-child(3) .date-range time:nth-child(2)");
-      data.id = slugify(`${data.school_name}-${data.course_name}`);
       return data;
     });
 
-    profile.courses = await querySelectorAll(page, ".courses__list > li", async (elem) => {
-      let data = { id: null };
+    profile.courses = await querySelectorAll(page, "[data-section='courses'] ul > li", async (elem) => {
+      let data = {};
       data.name = await querySelectorAttr(elem, ":scope > div > *:nth-child(1)");
-      data.id = slugify(data.name);
       return data;
     });
 
     profile.projects = await querySelectorAll(page, ".projects__list > li", async (elem) => {
-      let data = { id: null };
+      let data = {};
       data.name = await querySelectorAttr(elem, ":scope > div > *:nth-child(1)");
-      data.id = slugify(data.name);
       data.date_start = await querySelectorAttr(elem, ":scope > div > *:nth-child(2) .date-range time:nth-child(1)");
       data.date_final = await querySelectorAttr(elem, ":scope > div > *:nth-child(2) .date-range time:nth-child(2)");
       data.description = await querySelectorAttr(elem, ":scope > div > *:nth-child(3) > div");
@@ -89,9 +87,44 @@ export default async () => {
       return data;
     });
 
-    profile.languages = [];
+    profile.languages = await querySelectorAll(page, "[data-section='languages'] ul > li", async (elem) => {
+      let data = {};
+      data.name = await querySelectorAttr(elem, ":scope > div > *:nth-child(1)");
+      return data;
+    });
+
+    profile.skills = [
+      { name: "PHP", rating: 5 },
+      { name: "Laravel", rating: 4 },
+      { name: "MySQL", rating: 4 },
+      { name: "Javascript", rating: 4 },
+      { name: "Vue 3", rating: 5 },
+      { name: "Nuxt 3", rating: 5 },
+      { name: "Vuetify 3", rating: 5 },
+      { name: "Docker", rating: 3 },
+      { name: "Node JS", rating: 4 },
+      { name: "Wordpress", rating: 5 },
+      { name: "Three.js", rating: 3 },
+    ];
+
+    const generateIds = {
+      experience: ["experience", "company", "job"],
+      education: ["education", "school_name", "course_name"],
+      courses: ["course", "name"],
+      projects: ["project", "name"],
+      languages: ["language", "name"],
+      skills: ["skill", "name"],
+    };
+
+    Object.entries(generateIds).map(([name, fields]) => {
+      profile[name].map((item, index) => {
+        const id = slugify(fields.map((field) => item[field] || field).join("-"));
+        profile[name][index] = { id, ...item };
+      });
+    });
 
     fs.writeFileSync("./data/linkedin-resume.json", JSON.stringify(profile, null, 2));
+    console.log("ready");
   } catch (err) {
     console.log(err);
   }
