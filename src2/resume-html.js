6ae7adc9ebd3ee27838d838b4bc830a07c95d67e
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-09-24 12:42:22 -0300

    build

--- src2/resume-html.js
+++ src2/resume-html.js
@@ -5,25 +5,29 @@ export default async (data) => {
   let content = [];
 
   content.push(
-    `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Document</title></head><body>`,
+    `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Document</title>`,
+    `<style>* {font-family: monospace !important;}</style>`,
+    `</head><body>`,
   );
   content.push(`<div style="max-width:1000px; margin:0 auto; font-family: Segoe UI;">`);
 
   content.push(`<h1>${data.basics.name}</h1>`);
   content.push(`<h3>${data.basics.label}</h3>`);
   content.push(`<h3>${format.location(data.basics.location)}</h3>`);
-  content.push(`<br /><br />`);
+
+  content.push(`<br /><hr /><br />`);
 
   content.push(`<table><tbody>`);
   [...data.links, ...data.contacts].map((link, index) => {
     // content.push(`<div><a href="${link.url}" target="_blank">${link.name}: ${link.url}</a></div>`);
     content.push(`<tr>`);
-    content.push(`<td>${link.name} &nbsp;</td>`);
+    content.push(`<td valign="middle">${link.name} &nbsp;</td>`);
     content.push(`<td><a href="${link.url}" style="text-decoration:none;">${link.value || link.url}</a></td>`);
     content.push(`</tr>`);
   });
   content.push(`</tbody></table>`);
-  content.push(`<br /><br /><br />`);
+
+  content.push(`<br /><hr /><br />`);
 
   content.push(format.nl2br(data.basics.summary));
 
